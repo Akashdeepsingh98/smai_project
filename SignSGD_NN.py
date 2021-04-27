841e9efae7d117aee10e28c1a3070fe65f2d5a38
Akashdeep <akashdeepsingh98@gmail.com>
2021-04-27 22:01:55 +0530

    please do not touch anythin, I gotta fix it

--- SignSGD_NN.py
+++ SignSGD_NN.py
@@ -89,19 +89,35 @@ class DeepNeuralNetwork():
         params = self.params
         change_w = {}
 
+        # communicate all 3 gradients one by one
         # Calculate W3 update
-        error = 2 * (output - y_train) / \
-            output.shape[0] * self.softmax(params['Z3'], derivative=True)
+        grad3 = self.softmax(params['Z3'], derivative=True)
+        grad3[grad3 > 0] = 1
+        grad3[grad3 < 0] = -1
+        grad3[grad3 == 0] = 0
+        comm.Send(grad3, dest=0, tag=6)
+        comm.Recv(grad3, source=0, tag=9)
+        error = 2 * (output - y_train) / output.shape[0] * grad3
         change_w['W3'] = np.outer(error, params['A2'])
 
         # Calculate W2 update
-        error = np.dot(params['W3'].T, error) * \
-            self.sigmoid(params['Z2'], derivative=True)
+        grad2 = self.softmax(params['Z2'], derivative=True)
+        grad2[grad2 > 0] = 1
+        grad2[grad2 < 0] = -1
+        grad2[grad2 == 0] = 0
+        comm.Send(grad2, dest=0, tag=5)
+        comm.Recv(grad2, source=0, tag=8)
+        error = np.dot(params['W3'].T, error) * grad2
         change_w['W2'] = np.outer(error, params['A1'])
 
         # Calculate W1 update
-        error = np.dot(params['W2'].T, error) * \
-            self.sigmoid(params['Z1'], derivative=True)
+        grad1 = self.softmax(params['Z3'], derivative=True)
+        grad1[grad1 > 0] = 1
+        grad1[grad1 < 0] = -1
+        grad1[grad1 == 0] = 0
+        comm.Send(grad1, dest=0, tag=4)
+        comm.Recv(grad1, source=0, tag=7)
+        error = np.dot(params['W2'].T, error) * grad1
         change_w['W1'] = np.outer(error, params['A0'])
 
         return change_w
@@ -144,10 +160,10 @@ class DeepNeuralNetwork():
                 changes_to_w = self.backward_pass(y, output)
                 self.update_network_parameters(changes_to_w)
 
-            accuracy = self.compute_accuracy(x_val, y_val)
-            print('Epoch: {0}, Time Spent: {1:.2f}s, Accuracy: {2:.2f}%'.format(
-                iteration+1, time.time() - start_time, accuracy * 100
-            ))
+            #accuracy = self.compute_accuracy(x_val, y_val)
+            # print('Epoch: {0}, Time Spent: {1:.2f}s, Accuracy: {2:.2f}%'.format(
+            #    iteration+1, time.time() - start_time, accuracy * 100
+            # ))
 
 
 #dnn = DeepNeuralNetwork(sizes=[784, 128, 64, 10])
@@ -173,62 +189,57 @@ if rank == 0:
     comm.Send(mainNN.params['W1'], dest=3, tag=1)
     comm.Send(mainNN.params['W2'], dest=3, tag=2)
     comm.Send(mainNN.params['W3'], dest=3, tag=3)
-    
+
     for epoch in range(EPOCHS):
         for i in range(len(x_train)):
-            # grad signs of worker 1, W1
-            grad11 = np.empty(mainNN.params['W1'].shape)
-            comm.Recv(grad11, source=1, tag=4)
-            # grad signs of worker 1, W2
-            grad12 = np.empty(mainNN.params['W2'].shape)
-            comm.Recv(grad12, source=1, tag=5)
-            # grad signs of worker 1, W3
-            grad13 = np.empty(mainNN.params['W3'].shape)
+            grad13 = np.empty(SIZES[3])
             comm.Recv(grad13, source=1, tag=6)
-            # grad signs of worker 2, W1
-            grad21 = np.empty(mainNN.params['W1'].shape)
-            comm.Recv(grad21, source=2, tag=4)
-            grad22 = np.empty(mainNN.params['W2'].shape)  # and so on
-            comm.Recv(grad22, source=2, tag=5)
-            grad23 = np.empty(mainNN.params['W3'].shape)
+            grad23 = np.empty(SIZES[3])
             comm.Recv(grad23, source=2, tag=6)
-            grad31 = np.empty(mainNN.params['W1'].shape)
-            comm.Recv(grad31, source=3, tag=4)
-            grad32 = np.empty(mainNN.params['W2'].shape)
-            comm.Recv(grad32, source=3, tag=5)
-            grad33 = np.empty(mainNN.params['W3'].shape)
+            grad33 = np.empty(SIZES[3])
             comm.Recv(grad33, source=3, tag=6)
 
-            vote1 = np.zeros(mainNN.params['W1'])
-            vote2 = np.zeros(mainNN.params['W2'])
-            vote3 = np.zeros(mainNN.params['W3'])
-
-            vote1 = grad11+grad21+grad31
-            vote2 = grad12+grad22+grad32
             vote3 = grad13+grad23+grad33
-
-            vote1[vote1 > 0] = 1
-            vote1[vote1 < 0] = -1
-            vote1[vote1 == 0] = 0
-            vote2[vote2 > 0] = 1
-            vote2[vote2 < 0] = -1
-            vote2[vote2 == 0] = 0
             vote3[vote3 > 0] = 1
             vote3[vote3 < 0] = -1
             vote3[vote3 == 0] = 0
-
-            comm.Send(vote1, dest=1, tag=7)
-            comm.Send(vote1, dest=2, tag=7)
-            comm.Send(vote1, dest=3, tag=7)
-            comm.Send(vote2, dest=1, tag=8)
-            comm.Send(vote2, dest=2, tag=8)
-            comm.Send(vote2, dest=3, tag=8)
             comm.Send(vote3, dest=1, tag=9)
             comm.Send(vote3, dest=2, tag=9)
             comm.Send(vote3, dest=3, tag=9)
 
-            mainNN.update_network_parameters({'Z1':vote1})
+            grad12 = np.empty(SIZES[2])
+            comm.Recv(grad12, source=1, tag=5)
+            grad22 = np.empty(SIZES[2])
+            comm.Recv(grad22, source=2, tag=5)
+            grad32 = np.empty(SIZES[2])
+            comm.Recv(grad32, source=3, tag=5)
 
+            vote2 = grad12+grad22+grad32
+            vote2[vote2 > 0] = 1
+            vote2[vote2 < 0] = -1
+            vote2[vote2 == 0] = 0
+            comm.Send(vote2, dest=1, tag=8)
+            comm.Send(vote2, dest=2, tag=8)
+            comm.Send(vote2, dest=3, tag=8)
+
+            grad11 = np.empty(SIZES[1])
+            comm.Recv(grad11, source=1, tag=4)
+            grad21 = np.empty(SIZES[1])
+            comm.Recv(grad21, source=2, tag=4)
+            grad31 = np.empty(SIZES[1])
+            comm.Recv(grad31, source=3, tag=4)
+
+            vote1 = grad11+grad21+grad31
+            vote1[vote1 > 0] = 1
+            vote1[vote1 < 0] = -1
+            vote1[vote1 == 0] = 0
+            comm.Send(vote1, dest=1, tag=7)
+            comm.Send(vote1, dest=2, tag=7)
+            comm.Send(vote1, dest=3, tag=7)
+            mainNN.update_network_parameters({})
+
+        print('Epochs: {}'.format(epoch+1))
+        print(mainNN.compute_accuracy(x_val, y_val))
 
 elif rank == 1:
     nn1 = DeepNeuralNetwork(sizes=SIZES, epochs=EPOCHS, l_rate=L_RATE)
